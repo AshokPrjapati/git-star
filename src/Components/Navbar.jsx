732b6918a79bff44f007192b25cdc2f87c659d81
Ashok Kumar <ap271449@gmail.com>
2023-01-06 16:30:04 +0530

    fixed : undefined

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Flex, Text } from "@chakra-ui/react";
+import { Flex } from "@chakra-ui/react";
 import styles from "./Navbar.module.css";
 import { NavLink } from "react-router-dom";
 
